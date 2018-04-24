7bb303000cd372ca90cd39b110d296f6c9984667
Sarah <yamashige.sarah@gmail.com>
2018-04-23 22:06:44 -1000

    Show and hide navi elements.

--- js/app.js
+++ js/app.js
@@ -4,7 +4,21 @@
 
 /*1) to the navmenu items (Home, Location, Blog) that will show and hide the contents for that specific navmenu item.*/
 
+var menuItems = document.getElementsByClassName('navi');
 
+for (var i = 0; i < menuItems.length; i++) {
+    menuItems[i].addEventListener('click', showItem);
+}
+
+function showItem() {
+    var itemElem = this.querySelectorAll('.inner')[0];
+        if(itemElem.style.display === 'block') {
+            itemElem.style.display = 'none';
+        }
+        else {
+            itemElem.style.display = 'block';
+        }
+}
 
 
 
